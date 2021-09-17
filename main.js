173a11fde3e7c151540362bce47f146d30b517cb
11-osMatin <83844035+11-osMatin@users.noreply.github.com>
2021-09-17 16:00:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My First Console Project")
+
+var first_name = "My First Name is Matin";
+console.log(first_name)
+
+var last_name = "My Last Name is Manuchehri";
+console.log(last_name)
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = a / b;
+console.log(divide);
